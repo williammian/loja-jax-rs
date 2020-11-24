ab75061e05805146ed3a158c8c680234051f5d07
William Foleis Mian <william_mian@yahoo.com.br>
2020-11-24 01:14:49 -0200

    Teste after before

--- src/main/java/br/com/alura/loja/Servidor.java
+++ src/main/java/br/com/alura/loja/Servidor.java
@@ -8,10 +8,15 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Servidor {
 	
+	public static HttpServer inicializaServidor() {
+	    ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+	    URI uri = URI.create("http://localhost:8080/");
+	    HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+	    return server;
+	}
+	
 	public static void main(String[] args) throws Exception {
-        URI uri = URI.create("http://localhost:8080/");
-        ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		HttpServer server = inicializaServidor();
         System.out.println("Servidor rodando");
         System.in.read();
         server.stop();
--- src/test/java/br/com/alura/loja/CarrinhoTest.java
+++ src/test/java/br/com/alura/loja/CarrinhoTest.java
@@ -4,17 +4,37 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+
 public class CarrinhoTest {
+	
+	private HttpServer server;
+	
+	@Before
+    public void before() {
+        this.server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
 	
 	@Test
     public void testaQueAConexaoComOServidorFuncionaNoPathDeCarrinhos() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
         String conteudo = target.path("/carrinhos").request().get(String.class);
-        Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
     }
 
 }
