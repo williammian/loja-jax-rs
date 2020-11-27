8d9c1ae3cbc13d138c21c303c7cc0715ead4a2de
William Mian <william_mian@yahoo.com.br>
2020-11-27 13:30:14 -0300

    melhorando recurso Projeto

--- src/test/java/br/com/alura/loja/ProjetoTest.java
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -2,19 +2,62 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Projeto;
+
 public class ProjetoTest {
+	
+private HttpServer server;
+	
+	@Before
+    public void before() {
+        this.server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
 	
 	@Test
     public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
-        String conteudo = target.path("/projetos").request().get(String.class);
-        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+        String conteudo = target.path("/projetos/1").request().get(String.class);
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
+    }
+	
+	@Test
+    public void testaQueSuportaNovosProjetos(){
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        
+        Projeto projeto = new Projeto();
+        projeto.setAnoDeInicio(2020);
+        projeto.setNome("Curso");
+        String xml = projeto.toXML();
+
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/projetos").request().post(entity);
+        Assert.assertEquals(201, response.getStatus());
+        
+        String location = response.getHeaderString("Location");
+        String conteudo = client.target(location).request().get(String.class);
+        Assert.assertTrue(conteudo.contains("Curso"));
     }
 
 }
