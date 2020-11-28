96428c6864900c15459fbfebf15d87210ae04154
William Foleis Mian <william_mian@yahoo.com.br>
2020-11-28 16:02:21 -0200

    put

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -51,6 +53,26 @@ public class CarrinhoResource {
     	Carrinho carrinho = new CarrinhoDAO().busca(id);
         carrinho.remove(produtoId);
     	return Response.ok().build();
+    }
+    
+    @Path("{id}/produtos/{produtoId}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response alteraProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId, String conteudo) {
+    	Carrinho carrinho = new CarrinhoDAO().busca(id);
+        Produto produto = (Produto) new XStream().fromXML(conteudo);
+        carrinho.troca(produto);
+        return Response.ok().build();
+    }
+    
+    @Path("{id}/produtos/{produtoId}/quantidade")
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response trocaQuantidade(@PathParam("id") long id, @PathParam("produtoId") long produtoId, String conteudo) {
+        Carrinho carrinho = new CarrinhoDAO().busca(id);
+        Produto produto = (Produto) new XStream().fromXML(conteudo);
+        carrinho.trocaQuantidade(produto);
+        return Response.ok().build();
     }
 
 }
